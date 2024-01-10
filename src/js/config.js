264fd8df3381547243b2a7af9f16d454643b2a85
HastiFattahi <hastifattahii21@gmail.com>
2024-01-10 22:09:33 +0330

    Final commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL='https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC=20
+export const RES_PER_PAGE=10
+export const KEY='eae800b4-c4de-460c-8dac-faccb10a46ab'
+export const MODAL_CLOSE_SEC=2.5
